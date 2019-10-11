53eca7fbb597594526fe1731ad0594894614ef9f
ahoguegh <allan_hogue@yahoo.com>
2019-10-11 08:25:31 -0500

    revised guessinggame

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Purpose: Guess the number of files in the pwd.
 
-
+get_guess () {
 echo "How many files are in the current directory?"
 read
-files=`ls -l | grep ^- | wc -l`
+}
 
+files=`ls -l | grep ^- | wc -l`
+get_guess
 while true; do
     if [ $files -eq $REPLY ]; then
         echo "You are right!"; exit 0
     elif [ $REPLY -gt $files ]; then
         echo "You guessed too high..."
-        echo "How many files are in the current directory?"
-        read
+        get_guess
     else
         echo "You guessed too low..."
-        echo "How many files are in the current directory?"
-        read
+        get_guess
     fi
 done
 
